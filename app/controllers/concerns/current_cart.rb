cf2c10eafbbb65a72b06c01a91cb5d10254f0651
Santu Mahapatra <santumahapatra@gmail.com>
2014-07-02 12:31:10 +0530

    Fixing mistake in current_cart. Should be ActiveRecord instead of ActiveSupport.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -5,7 +5,7 @@ module CurrentCart
 
     def set_cart
       @cart = Cart.find(session[:cart_id])
-    rescue ActiveSupport::RecordNotFound
+    rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
       session[:cart_id] = @cart.id
     end
