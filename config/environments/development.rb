04ae78735b4c1a1c31022cc7ec1e29d0a8ab4bf4
Santu Mahapatra <santumahapatra@gmail.com>
2014-07-19 10:55:47 +0530

    Sending confirmation emails.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 end
